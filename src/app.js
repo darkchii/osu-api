863507497ce47fb215d147e0542bab2a790f2d36
Amayakase <engineermarkpro@gmail.com>
2022-08-12 18:39:39 +0200

    cors stuff

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+}));
 app.use(express.json());
 
 app.use('/', api);
