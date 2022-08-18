4d677c0bde7bdba29866adc5b13679040b51cdb2
Amayakase <engineermarkpro@gmail.com>
2022-08-18 23:13:03 +0200

    Added a proxy endpoint for https calls and beatmap count endpoint

--- src/api/beatmaps.js
+++ src/api/beatmaps.js
@@ -11,6 +11,76 @@ const connConfig = {
   password: process.env.MYSQL_PASS,
 };
 
+router.get('/count', async (req, res) => {
+  const connection = mysql.createConnection(connConfig);
+  const mode = req.query.mode !== undefined ? req.query.mode : 0;
+
+  connection.on('error', (err) => {
+    res.json({
+      message: 'Unable to connect to database',
+      error: err,
+    });
+  });
+
+  let q = 'WHERE mode=? AND (approved=1 OR approved=2)';
+  const qVar = [mode];
+
+  if (req.query.stars_min) {
+    q += ' AND star_rating>=?';
+    qVar.push(req.query.stars_min);
+  }
+  if (req.query.stars_max) {
+    q += ' AND star_rating<?';
+    qVar.push(req.query.stars_max);
+  }
+  if (req.query.ar_min) {
+    q += ' AND ar>=?';
+    qVar.push(req.query.ar_min);
+  }
+  if (req.query.ar_max) {
+    q += ' AND ar<?';
+    qVar.push(req.query.ar_max);
+  }
+  if (req.query.od_min) {
+    q += ' AND od>=?';
+    qVar.push(req.query.od_min);
+  }
+  if (req.query.od_max) {
+    q += ' AND od<?';
+    qVar.push(req.query.od_max);
+  }
+  if (req.query.cs_min) {
+    q += ' AND cs>=?';
+    qVar.push(req.query.cs_min);
+  }
+  if (req.query.cs_max) {
+    q += ' AND cs<?';
+    qVar.push(req.query.cs_max);
+  }
+  if (req.query.hp_min) {
+    q += ' AND hp>=?';
+    qVar.push(req.query.hp_min);
+  }
+  if (req.query.hp_max) {
+    q += ' AND hp<?';
+    qVar.push(req.query.hp_max);
+  }
+  if (req.query.length_min) {
+    q += ' AND total_length>=?';
+    qVar.push(req.query.length_min);
+  }
+  if (req.query.length_max) {
+    q += ' AND total_length<?';
+    qVar.push(req.query.length_max);
+  }
+
+  const result = await connection.awaitQuery(`SELECT COUNT(*) as amount FROM beatmap ${q}`, qVar);
+
+  res.json(result[0].amount);
+
+  await connection.end();
+});
+
 router.get('/monthly', async (req, res) => {
   const connection = mysql.createConnection(connConfig);
   const mode = req.query.mode !== undefined ? req.query.mode : 0;
@@ -32,6 +102,33 @@ router.get('/monthly', async (req, res) => {
   }
 
   const result = await connection.awaitQuery('SELECT MONTH(approved_date) as month, YEAR(approved_date) as year, SUM(total_length) as length, SUM(max_score) as score, COUNT(*) as amount FROM beatmap WHERE mode=? AND (approved=1 OR approved=2) GROUP BY YEAR(approved_date), MONTH(approved_date)', [mode]);
+
+  res.json(result);
+
+  await connection.end();
+});
+
+router.get('/yearly', async (req, res) => {
+  const connection = mysql.createConnection(connConfig);
+  const mode = req.query.mode !== undefined ? req.query.mode : 0;
+
+  connection.on('error', (err) => {
+    res.json({
+      message: 'Unable to connect to database',
+      error: err,
+    });
+  });
+
+  const months = [];
+
+  const _start = moment('6 Oct 2007');
+  // var _end = moment(sorted[sorted.length - 1].actual_date).add(1, `${addDateFormat}s`);
+  const _end = moment().add(1, 'years');
+  for (let m = moment(_start); m.isBefore(_end); m.add(1, 'years')) {
+    months.push(moment(m));
+  }
+
+  const result = await connection.awaitQuery('SELECT YEAR(approved_date) as year, SUM(total_length) as length, SUM(max_score) as score, COUNT(*) as amount FROM beatmap WHERE mode=? AND (approved=1 OR approved=2) GROUP BY YEAR(approved_date)', [mode]);
 
   res.json(result);
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const axios = require('axios');
 
 const beatmaps = require('./beatmaps');
 const user = require('./user');
 
 const router = express.Router();
 
+router.get('/proxy/:url', async (req, res) => {
+  const url = Buffer.from(req.params.url, 'base64').toString('utf-8');
+  const _res = await axios.get(url);
+
+  res.json(_res.data);
+});
+
 router.use('/beatmaps', beatmaps);
 router.use('/users', user);
+
 
 module.exports = router;
